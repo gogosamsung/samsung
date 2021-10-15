8773e12548a477281f68a420f5a92d9da742a8c2
RJ Kang <80829530+Paran-Yu@users.noreply.github.com>
2021-10-16 02:10:14 +0900

    211015_B1003_강리정

--- B1003/B1003_강리정.cpp
+++ B1003/B1003_강리정.cpp
@@ -1,1 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int cnt[41][2] = { 0 };
+
+int main() {
+	int T;
+	scanf("%d", &T);
+
+	cnt[0][0] = 1;
+	cnt[0][1] = 0;
+	cnt[1][0] = 0;
+	cnt[1][1] = 1;
+
+	for (int i = 2; i <= 40; i++) {
+		cnt[i][0] = cnt[i - 1][0] + cnt[i - 2][0];
+		cnt[i][1] = cnt[i - 1][1] + cnt[i - 2][1];
+	}
+
+	for (int tc = 1; tc <= T; tc++) {
+		int num;
+		scanf("%d", &num);
+
+		printf("%d %d\n", cnt[num][0], cnt[num][1]);
+	}
+
+	return 0;
+}
